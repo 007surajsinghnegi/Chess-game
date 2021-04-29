9037bb8033e0dd0a6cb076cbaf75ca9be5f649d5
unknown <1234surajsinghnegi@gmail.com>
2021-04-29 10:10:44 +0530

    First commit

---
+++ src/Piece.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {useDrag, DragPreviewImage} from 'react-dnd';
+
+
+function Piece({ piece , position }) {
+    const type= piece.type;
+    const color = piece.color;
+    const pieceImg = require(`./assets/${type}_${color}.png`).default
+
+    const [ {isDragging}, drag , preview] = useDrag({
+        type: 'piece',
+        item:{ id: `${position}_${type}_${color}`},
+        collect:(monitor) =>{
+            return {isDragging: !!monitor.isDragging()}
+        } 
+    })
+
+    // console.log(JSON.stringify(pieceImg))
+    return (
+        <>
+            <DragPreviewImage connect={preview} src={pieceImg}/>
+            <div className="piece-container" ref={drag} style={{opacity : isDragging ? 0:1}}>
+                <img src={pieceImg} />
+            </div>
+        </>
+        
+    )
+}
+
+export default Piece;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD2Wk9YAvUD5KCcbHl1kQKCxN0eIdsFVbc",
+    authDomain: "chess-game-9a9bc.firebaseapp.com",
+    projectId: "chess-game-9a9bc",
+    storageBucket: "chess-game-9a9bc.appspot.com",
+    messagingSenderId: "618854098404",
+    appId: "1:618854098404:web:d083bb28d1e915826b13f1",
+    measurementId: "G-JBJ1KQS9WB"
+  };
